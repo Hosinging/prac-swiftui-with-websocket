1e242a19caec14612923f62559258d775ee9e59d
Theo <>
2022-01-19 21:02:59 +0900

    feat: 프로젝트 초기화

---
+++ prac-swiftui-with-websocket/prac-swiftui-with-websocket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prac-swiftui-with-websocket
+//
+//  Created by Theo on 2022/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prac-swiftui-with-websocket/prac-swiftui-with-websocket/prac_swiftui_with_websocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  prac_swiftui_with_websocketApp.swift
+//  prac-swiftui-with-websocket
+//
+//  Created by Theo on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct prac_swiftui_with_websocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
